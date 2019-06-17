9d637ee5054f220a4e924090698b28ce62135744
Carlos Gonzales <carlos.gonzales@jalasoft.com>
2019-06-17 17:30:21 -0400

    pivotal create project test

--- src/test/java/com/jalasoft/pivotal/LoginTest.java
+++ src/test/java/com/jalasoft/pivotal/LoginTest.java
@@ -41,4 +41,44 @@ public class LoginTest {
 
         System.out.println("");
     }
+
+    @Test
+    public void testCreateProject() {
+
+        // Given
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.pivotaltracker.com/");
+
+        driver.findElement(By.cssSelector(".header__lg a[href=\"/blog\"] + a")).click();
+
+        String expectedUserName = "Carledriss";
+        driver.findElement(By.cssSelector("#credentials_username")).sendKeys(expectedUserName);
+
+        driver.findElement(By.cssSelector(".app_signin_action_button")).click();
+
+        String password = "P@ssw0rd";
+        driver.findElement(By.cssSelector("#credentials_password")).sendKeys(password);
+
+        driver.findElement(By.cssSelector(".app_signin_action_button")).click();
+
+        // When
+
+        driver.findElement(By.cssSelector("#create-project-button")).click();
+
+        driver.findElement(By.cssSelector("input[name=\"project_name\"]")).sendKeys("MyProject");
+
+        driver.findElement(By.cssSelector(".tc-account-selector__header")).click();
+
+        driver.findElement(By.xpath("//div[@class='tc-account-selector__option-account-name' and text()='account1']")).click();
+
+        driver.findElement(By.cssSelector("input[data-aid=\"public\"]")).click();
+
+        driver.findElement(By.cssSelector("[data-aid=\"FormModal__submit\"]")).click();
+
+        // Then
+
+
+    }
 }
