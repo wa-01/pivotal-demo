dfbfeeb13d3a731e9d1d982db7e3b8f306b34f4e
Carlos Gonzales <carlos.gonzales@jalasoft.com>
2019-07-10 17:55:06 -0400

    Adding Driver Factory

---
+++ src/main/java/com/jalasoft/pivotal/core/ui/DriverFactory.java
@@ -0,0 +1,30 @@
+package com.jalasoft.pivotal.core.ui;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class DriverFactory {
+
+    private static final Map<String, Supplier<AbstractDriver>> DRIVERS = new HashMap<>();
+    static {
+        DRIVERS.put("chrome", Chrome::new);
+        DRIVERS.put("firefox", Firefox::new);
+        DRIVERS.put("remote", BrowserStack::new);
+        DRIVERS.put("docker", BrowserStack::new);
+        DRIVERS.put("headless", BrowserStack::new);
+    }
+
+    public static WebDriver getDriver(String browser) {
+		return DRIVERS.get(browser).get().initDriver();
+    }
+}
--- src/test/resources/features/projects.feature
+++ src/test/resources/features/projects.feature
@@ -4,14 +4,7 @@ Feature: Projects
     Given I sign in as "Carledriss" user and "P@ssw0rd"
     When I click the new project button
     And I set the project form
-      | name    | MyProject24 |
+      | name    | MyProject26 |
       | account | account1    |
       | privacy | public      |
-    Then I validate the project label is "MyProject24"
-
-
-
-#    And I set the project form
-#      | name        | account  | privacy |
-#      | MyProject22 | account1 | public  |
-#      | MyProject23 | account2 | private  |
+    Then I validate the project label is "MyProject26"
