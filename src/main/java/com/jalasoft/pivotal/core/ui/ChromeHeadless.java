680eb25487bea7facc593f02218122dd1e2889d5
carledriss <carledriss@gmail.com>
2020-02-26 15:15:57 -0400

    Support parallel execution

--- src/main/java/com/jalasoft/pivotal/core/ui/ChromeHeadless.java
+++ src/main/java/com/jalasoft/pivotal/core/ui/ChromeHeadless.java
@@ -1,6 +1,7 @@
 package com.jalasoft.pivotal.core.ui;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,6 +13,8 @@ public class ChromeHeadless extends AbstractDriver{
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
         return new ChromeDriver(options);
     }
 }
--- src/main/java/com/jalasoft/pivotal/core/ui/SharedDriver.java
+++ src/main/java/com/jalasoft/pivotal/core/ui/SharedDriver.java
@@ -9,7 +9,7 @@ public class SharedDriver {
 
     public SharedDriver() {
         if (DriverFactory.getDriver() == null) {
-            String browser = Environment.getInstance().getValue("$['browser']").toUpperCase();
+            String browser = Environment.getInstance().getValue("$['browser']");
             WebDriver driver = BrowserFactory.getDriver(browser);
             driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
             driver.manage().window().maximize();
